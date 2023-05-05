6bb654dd332e408e7355d422388190d734927b17
Kenichi Ishigaki <ishigaki@cpan.org>
2023-05-06 00:19:25 +0900

    Show content with some errors

--- t/test_classes/Test/Pause99/Web/ChangePassword.pm
+++ t/test_classes/Test/Pause99/Web/ChangePassword.pm
@@ -24,7 +24,7 @@ sub test_basic : Tests(5) {
     # Now try a matching password
     $m->change_passwd->change_passwd__submit( 'moo', 'moo' );
     $m->mech->content_like( qr/New password stored and enabled/,
-        "New password message shown" );
+        "New password message shown" ) or diag $m->mech->content;
 
     # 401 if we re-use old credentials
     $m->homepage;
